f806a2461650dadab2ecd61512d7434ff0e221bd
Loy Yee Ko <koloyyee@gmail.com>
2025-02-03 16:26:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# What's this?
+
+A collection of my Java script (not javascript) with [jbang](<https://www.jbang.dev/#> What's this?)
---
+++ domains/ExtractCsv.java
@@ -0,0 +1,41 @@
+
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS de.siegmar:fastcsv:3.4.0
+//DEPS com.alibaba.fastjson2:fastjson2:2.0.54
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.alibaba.fastjson2.JSON;
+
+import de.siegmar.fastcsv.reader.CsvReader;
+import de.siegmar.fastcsv.reader.CsvRecord;
+
+public class ExtractCsv {
+
+    public static void main(String... args) throws IOException {
+        Path file = Paths.get(System.getProperty("user.dir"), "free-domain.csv");
+
+        try (CsvReader<CsvRecord> csv = CsvReader.builder().ofCsvRecord(file)) {
+            var list = csv.stream().map(rec -> rec.getFields().get(0)).toList();
+
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("email_domains", list);
+
+            String jsonString = JSON.toJSONString(jsonMap);
+
+            Path jsonFile = Paths.get("domains.json");
+            try {
+                Files.write(jsonFile, jsonString.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
